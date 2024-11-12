cbef710bb37aae79d65a10295f087d5478e4b921
Max Garanin <garanin@mercurydevelopment.com>
2024-11-12 17:20:56 +0400

    Minor updates for  ECDsa

--- CommonDotNet/Cryptography/EcdsaGenerator.cs
+++ CommonDotNet/Cryptography/EcdsaGenerator.cs
@@ -1,13 +1,11 @@
 ﻿using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 
-ECDsa ecdsa = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+using ECDsa ecdsa = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
     ? new ECDsaCng()
     : new ECDsaOpenSsl();
 
 ecdsa.GenerateKey(ECCurve.CreateFromFriendlyName("ECDSA_P384"));
 
-Console.WriteLine($"-----Private key-----{Environment.NewLine}{Convert.ToBase64String(ecdsa.ExportPkcs8PrivateKey())}{Environment.NewLine}");
-Console.WriteLine($"-----Public key-----{Environment.NewLine}{Convert.ToBase64String(ecdsa.ExportSubjectPublicKeyInfo())}");
-
-ecdsa.Dispose();
+Console.WriteLine($"-----Private key-----{Environment.NewLine}{ecdsa.ExportPkcs8PrivateKeyPem()}{Environment.NewLine}");
+Console.WriteLine($"-----Public key-----{Environment.NewLine}{ecdsa.ExportSubjectPublicKeyInfoPem()}");
